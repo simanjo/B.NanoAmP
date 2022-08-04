9f9c35faa161bca2bbae062a2a6a1c6e5dfa10cc
Jonathan Simantzik <82642377+simanjo@users.noreply.github.com>
2022-08-04 20:44:15 +0200

    added model file

---
+++ model.py
@@ -0,0 +1,14 @@
+def _get_flow_cells():
+    return []
+
+def _get_devices():
+    return []
+
+def _get_guppy_versions():
+    return []
+
+def _get_models():
+    return ["r104_e81_sup_g5015"]
+
+def _get_assemblers():
+    return ["Flye", "Raven", "Miniasm"]
