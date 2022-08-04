6b6ba6fde4fa7972b50e12ab374c3c418878e412
Jonathan Simantzik <82642377+simanjo@users.noreply.github.com>
2022-08-04 19:59:10 +0200

    added app init stub and main window

--- main.py
+++ main.py
@@ -2,9 +2,34 @@
 
 import dearpygui.dearpygui as dpg
 
+import view
+
+def _init_app():
+
+    view.add_file_dialog()
+    view.add_main_window()
+
+
+def _start_app():
+    dpg.create_viewport(title='B.NanoAmP', width=850, height=800)
+    dpg.setup_dearpygui()
+
+    dpg.show_viewport()
+    dpg.set_primary_window("main_window", True)
+
+    dpg.start_dearpygui()
+
 
 def main():
-    pass
+
+
+    dpg.create_context()
+
+    _init_app()
+    _start_app()
+
+    dpg.destroy_context()
+
 
 if __name__ == '__main__':
     main()
---
+++ view.py
@@ -0,0 +1,8 @@
+import dearpygui.dearpygui as dpg
+
+def add_file_dialog():
+    pass
+
+def add_main_window():
+    with dpg.window(tag="main_window", autosize=True, no_close=True, no_collapse=True):
+        pass
