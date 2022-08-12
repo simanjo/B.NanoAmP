935aca86c8106d78ba7962aa90195257bbd5cf0c
Jonathan Simantzik <82642377+simanjo@users.noreply.github.com>
2022-08-12 16:56:23 +0200

    simplified fasta path building for medaka call

--- PipelineSteps.py
+++ PipelineSteps.py
@@ -256,7 +256,7 @@ class RaconPolishingStep(PipelineStep):
             polishing_call, stdout=subprocess.PIPE, env=env
         )
         polish_out = os.path.join(
-           polish_dir, "racon.fasta"
+           polish_dir, "assembly.fasta"
         )
         with open(polish_out, 'wb') as out_fh:
             # ugly HACK?
@@ -436,20 +436,21 @@ def _get_racon_call(threads, prefix):
 
 def _get_medaka_call(threads, assembler, mod, is_racon, prefix):
     dirname = os.path.split(prefix)[1]
-    fasta = None
+    fasta_folder = None
     if assembler == "Flye":
         if is_racon:
-            fasta = os.path.join(prefix, "racon_polishing", "racon.fasta")
+            fasta_folder = "racon_polishing"
         else:
-            fasta = os.path.join(prefix, f"{dirname}_flye_assembly", "assembly.fasta")
+            fasta_folder = f"{dirname}_flye_assembly"
     elif assembler == "Raven":
-        fasta = os.path.join(prefix, f"{dirname}_raven_assembly", "assembly.fasta")
+        fasta_folder = f"{dirname}_raven_assembly"
     elif assembler == "Miniasm":
-        fasta = os.path.join(prefix, f"{dirname}_miniasm_assembly", "assembly.fasta")
+        fasta_folder = f"{dirname}_miniasm_assembly",
     else:
         raise NotImplemented(
             f"The assembly method {assembler} is not supported."
         )
+    fasta = os.path.join(prefix, fasta_folder, "assembly.fasta")
 
     medaka = [
         "medaka_consensus",
